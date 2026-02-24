07e6a73a479a3ea42a30e7bca0caa1d188f93008
anubhavsikka16 <anubhavsikka@gmail.com>
2026-02-24 13:03:26 +0530

    Adding log utility

--- utilities/log_utility.py
+++ utilities/log_utility.py
@@ -21,7 +21,7 @@ class Logger:
         self.logger.setLevel(file_level)
         fmt=logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
         current_time=time.strftime("%Y-%m-%d %H:%M:%S")
-        self.LogFileName='/Users/anubhavsikka/Documents/Assessment-twitch-selenium-framework/assessment-twitch-selenium-pytest/logs/log'+ current_time +'.log'
+        self.LogFileName='/Users/anubhavsikka/Documents/assessment-twitch-selenium-pytest/logs/log'+ current_time +'.log'
         sh=logging.FileHandler(self.LogFileName)
         sh.setLevel(file_level)
         sh.setFormatter(fmt)
